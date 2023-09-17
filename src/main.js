17c9a133fdad8ff1d3d3da8f25c7184fecfe28ca
Abhicode1507 <code.abhinavyadav1507@gmail.com>
2023-09-17 09:56:34 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 createApp(App).mount('#app')
